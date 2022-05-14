8189a14d31dc085f2fb1000e722dfb0f11a30453
developerjm_laptop <whdals1997@gmail.com>
2022-05-14 17:08:03 +0900

    Add Sign In Function;wq

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import React, {useState} from 'react';
 import './styles.css';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import { DialogActions, DialogContent, DialogContentText } from '@material-ui/core';
-import { StylesContext } from '@material-ui/core/node_modules/@material-ui/styles';
-import { SyncOutlined } from '@material-ui/icons';
-
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 
 
 function Header() {
   return (
     <header>
-      <h1>Welcome To Clubverse!</h1>
+      <Typography component="h1" variant="h3">
+        ClubVerse
+      </Typography> 
     </header>
   );
 }
 
-function LogIn() {
+function SignIn() {
   
-  // return(
+  return(
+    <div>
+      <Box sx={{
+            marginTop: 5,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginBottom: 5
+          }}>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h4" sx={{mb: 3}}>
+          Welcome to ClubVerse
+        </Typography>    
+        <TextField 
+          label="Email Address" 
+          fullWidth 
+          required 
+          name="email"
+          autoComplete="email"
+          sx={{mt:1}}
+        />
+        <TextField 
+          label="password" 
+          fullWidth 
+          required 
+          name="password"
+          autoComplete="password"
+          sx={{mt:1}}
+        />  
+        <Button type="submit" fullWidth variant="contained" sx={{mt:3, mb:2}}>Sign In</Button>
+        <Grid container>
+          <Grid item xs>
+            <Link>Forgot Password?</Link>
+          </Grid>
+          <Grid item>
+            <Link>Sign up</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
 
-  // );
+  );
 }
 
 // function Subscribe() {
@@ -36,21 +81,17 @@ function LogIn() {
 function Nav() {
   const [logOpen, setLogOpen] = useState(false);
   return (
-    <nav /*style={{ border: '1px solid grey'}}*/>
+    <nav>
       <h3>
         Menu
       </h3>
       <Button variant='outlined' onClick={()=>{setLogOpen(true)}}>로그인</Button>
       <Dialog open={logOpen}>
-        <DialogTitle>
-          Please Enter Your Email and Password
-        </DialogTitle>
         <DialogContent>
-          <Button variant='contained'>Log in</Button>
+          <SignIn></SignIn>
           <Button variant onClick={()=>{setLogOpen(false)}}>Cancel</Button>
         </DialogContent>
       </Dialog>
-      <LogIn></LogIn>
       <Button variant='outlined'>회원가입</Button><br />
       <Button variant='text' style={{color: 'black'}}>소모임 찾기</Button><br />
       <Button variant='text' style={{color: 'black'}}>동아리 찾기</Button><br />
@@ -64,7 +105,7 @@ function Article() {
   const [open, setOpen] = useState(false);
   return (
     <article >
-      <h2>Click below to enter metaverse</h2>
+      <h2>This is Clubverse main page</h2>
       <p>This Website is founded by developerjm for the first prototype of Clubverse webpage.</p>
       <br />
       <Button variant='contained' onClick={()=>{setOpen(true)} }>Enter</Button>
